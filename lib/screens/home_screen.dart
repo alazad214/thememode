0a15bce17ef624434809c1649ace33537788a42a
Al Azad <alazad214@gmail.com>
2024-05-06 11:38:44 +0600

    home screen added

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Theme"),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+    );
+  }
+}
